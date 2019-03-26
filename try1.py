d21cb5c4e5721cbd88e17e3095d6e8b3da207f2c
Ksen4ik <ksusha.tsoy@gmail.com>
2019-03-26 10:08:03 +0600

    Merge branch 'master' of /home/kbelotserkovets/Documents/Test with conflicts.

--- try1.py
+++ try1.py
@@ -4,8 +4,8 @@ import sys
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
-# from selenium.webdriver.support.ui import WebDriverWait
-# from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 current_dir = os.path.dirname(__file__)
 
@@ -24,26 +24,18 @@ browser.get('http://www.google.com')
 elem = browser.find_element_by_name('q')  # Find the search box
 elem.send_keys('Selenide' + Keys.RETURN)  # Input word "Selenide" and start search
 
-#  Проверка результатов поиска
-search_result = browser.find_elements(
-     by=By.CSS_SELECTOR,
-     value='#rso .srg div.g'
-    )
-assert len(search_result) > 0
-for result in search_result:
-    title_text = result.find_element_by_tag_name('h3').text
-    assert 'Selenide' in title_text
-
-
-# RESULTS_LOCATOR = "//div/h3/a"
-#
-# WebDriverWait(browser, 10).until(
-#     EC.visibility_of_element_located((By.XPATH, RESULTS_LOCATOR)))
-#
-# page1_results = browser.find_elements(By.XPATH, RESULTS_LOCATOR)
-#
-# for item in page1_results:
-#     print(item.text)
 
 assert 'Google' in browser.title
-# browser.quit() # This is optional, for closing browser
+assert "Selenide" in browser.page_source
+
+RESULTS_LOCATOR = "//*[@id="rso"]/div[1]/div/div/div/div/div[1]"
+
+WebDriverWait(browser, 10).until(
+    EC.visibility_of_element_located((By.XPATH, RESULTS_LOCATOR)))
+
+page1_results = browser.find_elements(By.XPATH, RESULTS_LOCATOR)
+
+for item in page1_results:
+    print(item.text)
+
+# browser.quit() # This is optional
