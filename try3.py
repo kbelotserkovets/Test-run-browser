fa27565a5706a0ba8cbf7c720dc7c964af26b8f6
Ksen4ik <ksusha.tsoy@gmail.com>
2019-04-04 14:08:55 +0600

    Added test

--- try3.py
+++ try3.py
@@ -8,7 +8,7 @@ class BaseTest(unittest.TestCase):
   def setUp(self):
     self.driver = webdriver.Chrome(executable_path="./chromedriver")
 
-  def testEpisodeContainsUrl(self):
+  def testFilm(self):
     driver = self.driver
     driver.implicitly_wait(5)
     driver.get('https://oc.kg/#/catalog/genre/37/order/1/page/1')
@@ -17,11 +17,17 @@ class BaseTest(unittest.TestCase):
 
     for film in films:
       name = film.find_element_by_css_selector('div.title a').text
-      year = re.search('\(.*\)', film.find_element_by_css_selector('div.subtitle').text).group()
+      year = re.search('([\d]+)', film.find_element_by_css_selector('div.subtitle').text).group()
       link = film.find_element_by_css_selector('div a').get_attribute('href')
-      print(name, year, link)
 
+      print('Russian name: {name}, Year: {year}, Link: {link}'.format(name=name, year=year, link=link))
 
+      self.assertNotEqual(0, len(name), "Expectation: The film's name in Russian should contain string ")
+
+      self.assertTrue(year.isdigit(), "Check the year contains only digits! :)")
+      self.assertEqual(4, len(year), "Expectation: The length of digits in year should be '4'")
+
+      self.assertIn("https://oc.kg/movie.php?id", link, "The link should contains: 'https://oc.kg/movie.php?id'")
 
 
   def tearDown(self):
