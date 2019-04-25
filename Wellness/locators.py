f7954aa0faf8004767a1b69fced3262c1fbb8fa0
Ksen4ik <ksusha.tsoy@gmail.com>
2019-04-25 19:46:16 +0600

    One test case added

--- Wellness/locators.py
+++ Wellness/locators.py
@@ -1,7 +1,17 @@
 from selenium.webdriver.common.by import By
 
-class LoginPageLocatars(object):
-  EMAIL         = (By.ID, 'email--1')
-  PASSWORD      = (By.ID, 'password--2')
-  SIGN_IN       = (By.CSS_SELECTOR, '[class*="sign_in_button"]')
-  ERROR_MESSAGE = (By.CSS_SELECTOR, '[class*="error__"]')
+class LoginPageLocators(object):
+
+    EMAIL         = (By.ID, 'email--1')
+    PASSWORD      = (By.ID, 'password--2')
+    SIGN_IN       = (By.CSS_SELECTOR, '[class*="sign_in_button"]')
+    ERROR_MESSAGE = (By.CSS_SELECTOR, '[class*="error__"]')
+
+class SettingsPageLocators(object):
+
+    FIRST_NAME    = (By.ID, 'firstName--3')
+    LAST_NAME     = (By.ID, 'lastName--4')
+    AGE           = (By.ID, 'age--5')
+    GENDER_MALE   = (By.CSS_SELECTOR, 'value="Male"')
+    GENDER_FEMALE = (By.CSS_SELECTOR, 'value="Female"')
+    SAVE_CHANGES  = (By.CSS_SELECTOR, '[class*="button_save_changes"]')
--- Wellness/login_test.py
+++ Wellness/login_test.py
@@ -11,7 +11,7 @@ class Login_Page_Test(unittest.TestCase):
     def setUp(self):
         self.driver = webdriver.Chrome(executable_path="./chromedriver")
 
-    def move_to_element(self, element):
+    def hover_element(self, element):
         hover = ActionChains(self.driver).move_to_element(element)
         hover.perform()
 
@@ -54,21 +54,49 @@ class Login_Page_Test(unittest.TestCase):
         self.assertIn(error_message, 'Invalid email or password, please try again')
 
 
-    def test_login_with_empty_field(self):
+    # def test_login_with_empty_field(self):
+    #     driver = self.driver
+    #     driver.maximize_window()
+    #     driver.implicitly_wait(5)
+    #     driver.get('https://staging.onestopwellness.ai/')
+    #
+    #     username = driver.find_element_by_id("email--1")
+    #     password = driver.find_element_by_id("password--2")
+    #     #
+    #     # username.send_keys("support@onestopwellness.ai")
+    #     # password.send_keys("password")
+    #
+    #     driver.find_element_by_css_selector('[class*="sign_in_button"]').click()
+    #
+    #     error_class = driver.find_elements_by_css_selector('[class*="custom_error"]')
+    #     #
+    #     # WebDriverWait(driver, 10).until(
+    #     #     EC.visibility_of_all_elements_located(error_class))
+    #
+    #     for error in error_class:
+    #         self.assertIn(error, username)
+    #         self.assertIn(error, password)
+
+
+    def test_change_user_name(self):
         driver = self.driver
         driver.maximize_window()
         driver.implicitly_wait(5)
         driver.get('https://staging.onestopwellness.ai/')
 
-        # username = driver.find_element_by_id("email--1")
-        # password = driver.find_element_by_id("password--2")
-        #
-        # username.send_keys("support@onestopwellness.ai")
-        # password.send_keys("password")
+        username = driver.find_element_by_id("email--1")
+        password = driver.find_element_by_id("password--2")
+
+        username.send_keys("support@onestopwellness.ai")
+        password.send_keys("5SdaG12pY2t0")
 
         driver.find_element_by_css_selector('[class*="sign_in_button"]').click()
 
+        user_photo = driver.find_element_by_css_selector('')
+        header > div > div[
 
+        class *="right_column_menu"] > div[class *= "user_information"] > div[class *= "user_profile] > div[class*="user_photo"]
+        self.hover_element()
 
 
     def tearDown(self):
